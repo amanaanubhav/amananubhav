ac4f024704106694ae9a05f2231e01450820c8b6
Aman Anubhav <amannbhv.cswork@gmail.com>
2025-12-08 15:25:09 +0530

    initialised web analytics

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { Analytics } from "@vercel/analytics/next"
 import { onAuthStateChanged, signInAnonymously } from 'firebase/auth';
 import { auth } from './Utils/firebase';
 
