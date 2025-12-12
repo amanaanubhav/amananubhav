8b2963ee391908c6a8f03eedee145d063778ebc7
Aman Anubhav <amannbhv.cswork@gmail.com>
2025-12-12 13:08:24 +0530

    initiated vercel speed insight

--- src/Components/Layout/Layout.jsx
+++ src/Components/Layout/Layout.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/react"
 import Navbar from './Navbar';
 import Footer from './Footer';
 import Preloader from '../UI/Preloader';
